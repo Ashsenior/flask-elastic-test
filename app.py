adbe7403d261db7f10234853413c48e15b2f4ca8
Ashutosh Bisht <49332381+Ashsenior@users.noreply.github.com>
2023-10-31 15:16:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,19 @@ app = Flask(__name__)
 def elasticsearch_webhook():
     # Print the received JSON data
     data = request.get_json()
+
+    lat_lon_str = data["lat_lon_data"]
+
+    # Remove the outermost double quotes from the string
+    lat_lon_str = lat_lon_str[1:-1]
+    
+    # Split the string into individual lists
+    lat_lon_list = [list(map(float, item.strip("[]").split(','))) for item in lat_lon_str.split("], [")]
+    
+    # Now, lat_lon_list contains the list of latitudes and longitudes as lists of floats
+    for index, lat_lon in enumerate(lat_lon_list):
+        print(f"Index {index}: Latitude {lat_lon[0]}, Longitude {lat_lon[1]}")
+    
     print("Received data from Elasticsearch:")
     print(data)
 

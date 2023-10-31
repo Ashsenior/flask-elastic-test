717c127c7e4b25262ca2d1e4a3720bb65f40c40b
Ashutosh Bisht <49332381+Ashsenior@users.noreply.github.com>
2023-10-31 14:29:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name)
+
+# Create an endpoint to handle incoming requests
+@app.route('/elasticsearch-webhook', methods=['POST'])
+def elasticsearch_webhook():
+    # Print the received JSON data
+    data = request.get_json()
+    print("Received data from Elasticsearch:")
+    print(data)
+
+    return 'Request received', 200
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
